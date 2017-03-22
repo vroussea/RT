30cf9c70604e32bbc3269494a5d118a5424af5a5
Gallien BOUDRIE <gboudrie@e3r11p15.42.fr>
2017-03-22 14:38:37 +0100

    every texture application works, time to merge

--- srcs/textures.c
+++ srcs/textures.c
@@ -6,7 +6,7 @@
 /*   By: gboudrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 16:30:29 by gboudrie          #+#    #+#             */
-/*   Updated: 2017/03/21 19:10:03 by gboudrie         ###   ########.fr       */
+/*   Updated: 2017/03/22 14:36:40 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,13 @@ int		texture_sphere(t_obj obj)
 	unsigned int	color;
 	double			p;
 	double			t;
-	int	x;
-	int	y;
+	int				x;
+	int				y;
 
 	p = PI + acos(obj.intersec_point[2] / obj.size);
 	t = PI + atan(obj.intersec_point[0] / obj.intersec_point[1]);
-	x = (int)((obj.texture->w * p) / 2 * PI);
-	y = (int)((obj.texture->h * t) / 2 * PI);
-	printf("x : %lf, y : %lf\n", p, t);
+	x = (int)((obj.texture->w * p * 6) / 2 * PI);
+	y = (int)((obj.texture->h * t * 6) / 2 * PI);
 	color = getpixel(obj.texture,
 					 abs(x % (int)obj.texture->w),
 					 abs(y % (int)obj.texture->h));
@@ -38,11 +37,16 @@ int		texture_sphere(t_obj obj)
 int		texture_cylinder(t_obj obj)
 {
 	unsigned int	color;
+	double			t;
+	double			z;
+	int				x;
 
+	t = PI + atan(obj.intersec_point[1] / obj.intersec_point[0]);
+	z = obj.intersec_point[2];
+	x = (int)((obj.texture->h * t * 6) / 2 * PI);
 	color = getpixel(obj.texture,
-					 abs((int)atan(obj.intersec_point[1] / obj.intersec_point[0])
-						 % (int)obj.texture->w),
-					 abs((int)obj.intersec_point[2] % (int)obj.texture->h));
+					 abs(x % (int)obj.texture->w),
+					 abs((int)z % (int)obj.texture->h));
 	return ((int)color);
 }
 
@@ -52,8 +56,8 @@ int		texture_plane(t_obj obj)
 	int	x;
 	int	y;
 
-	x = abs((int)(obj.intersec_point[0] * 8));
-	y = abs((int)(obj.intersec_point[1] * 8));
+	x = abs((int)(obj.intersec_point[0] * 16));
+	y = abs((int)(obj.intersec_point[1] * 16));
 	color = getpixel(obj.texture,
 					 x % (int)obj.texture->w,
 					 y % (int)obj.texture->h);
